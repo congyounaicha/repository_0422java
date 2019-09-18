b3e9428d9e766c15711a3a4b650ae6263c2ca80f
nepoleon <1210697542@qq.com>
2019-09-18 14:44:02 +0800

    edit a java

--- SpringbootApplication.java
+++ SpringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot001Application {
-
+//第二次修改 by me
     public static void main(String[] args) {
         SpringApplication.run(Springboot001Application.class, args);
     }
